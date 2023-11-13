74e4f0b82475f760a412825c09cb1fb1fe2e8706
socratic617 <erik.solis02135@gmail.com>
2023-11-12 23:39:36 -0500

    UPDATED PGS

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 module.exports = function(app, passport, db) {
 
+   const {ObjectId} = require('mongodb');//my string JOURNAL ID(from monogoDB) needs to be converted into an object aka "{ObjectId}""
 // normal routes ===============================================================
 
     // show the home page (will also have our login links)
@@ -7,33 +8,11 @@ module.exports = function(app, passport, db) {
         res.render('index.ejs');
     });
 
-    // community-page SECTION =========================
-    app.get('/community-page', isLoggedIn, function(req, res) { // checks to see if logged in or send them back to homepage if not logged in 
-        db.collection('messages').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          res.render('community-page.ejs', {
-            user : req.user,//showcases there usr name when logged in
-            messages: result
-          })
-        })
-    });
-
-    // JOURNAL ENTRY SECTION =========================
-    app.get('/journal-entry', isLoggedIn, function(req, res) { // checks to see if logged in or send them back to homepage if not logged in 
-      console.log('journal-entry')
-        db.collection('messages').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          res.render('journal-entry.ejs', {
-            user : req.user,//showcases there usr name when logged in
-            messages: result
-          })
-        })
-    });
 
         // MY JOURNALS ENTRIES SECTION =========================
     app.get('/my-journals', isLoggedIn, function(req, res) { // checks to see if logged in or send them back to homepage if not logged in 
       console.log('my-journals')
-        db.collection('messages').find().toArray((err, result) => {
+        db.collection('journal-entry').find().toArray((err, result) => {
           if (err) return console.log(err)
           res.render('my-journals.ejs', {
             user : req.user,//showcases there usr name when logged in
@@ -45,7 +24,7 @@ module.exports = function(app, passport, db) {
         // VISUALIZATION SECTION =========================
     app.get('/visualization', isLoggedIn, function(req, res) { // checks to see if logged in or send them back to homepage if not logged in 
       console.log('visualization')
-        db.collection('messages').find().toArray((err, result) => {
+        db.collection('journal-entry').find().toArray((err, result) => {
           if (err) return console.log(err)
           res.render('visualization.ejs', {
             user : req.user,//showcases there usr name when logged in
@@ -74,56 +53,170 @@ module.exports = function(app, passport, db) {
         res.redirect('/');
     });
 
-// message board routes ===============================================================
 
-    app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/community-page')
-      })
-    })
+  // =============================================================================
+  // COMMUNITY PAGE ENDPOINT ==================================================
+  // =============================================================================
 
-    app.put('/messages', (req, res) => {
+    // Get journal entries =========================
+    app.get('/community-page', isLoggedIn, function(req, res) { // checks to see if logged in or send them back to homepage if not logged in 
+        db.collection('journal-entry').find().toArray((err, result) => {
+          console.log("I AM HERE IN COMMUNITY PAGE (GET)")
+          console.log(result)
+          if (err) return console.log(err)
+          res.render('community-page.ejs', {
+            user : req.user,//showcases there usr name when logged in
+            entries: result
+          })
+        })
+    });
 
-      let startCounterThumbUp = 0;
-
+    app.put('/community-page', (req, res) => {
+  
       console.log(" (put method) : ")
-      console.log(req.body.thumbUp)
-      if(req.body.thumbUp !== undefined){
-      console.log("thumb up selected")
-      startCounterThumbUp  = req.body.thumbUp == null ? 0 : req.body.thumbUp + 1;//ternary deals with like to handle "NaN" aka null in JS deal with it by adding 1 to it or setting the new list item to 0
-    
-    } else if(req.body.thumbDown !== undefined ){
+      console.log(req.body)
 
-      console.log("thumb down(before): " + req.body.thumbDown)
-      startCounterThumbUp = req.body.thumbDown == null ? 0 : req.body.thumbDown - 1;
-      console.log("thumb down(after): " + startCounterThumbUp)
 
-   //ternary deals with dislike to handle "NaN" aka null in JS deal with it by subtract 1 to it or setting the new list item to 0
-      console.log("user selected thumb down");
-    }
-      db.collection('messages')
-      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-        $set: {
-          thumbUp:startCounterThumbUp,
-        }
-      }, {
-        sort: {_id: -1},
-        upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
-      })
+      db.collection('journal-entry')
+      .find({_id: ObjectId(req.body.id)})
+      .toArray((err, result) => {
+          if (err) return console.log(err)
+          console.log("result : ", result)
+
+          console.log(result[0].reactions[req.body.loggedInUserId])
+
+          if(result[0].reactions[req.body.loggedInUserId] !== undefined){
+            console.log('i need to remove the user id  ')
+            delete result[0].reactions[req.body.loggedInUserId]; //credit: https://stackoverflow.com/questions/3455405/how-do-i-remove-a-key-from-a-javascript-object
+            //im removing the star if I dont want to star it 
+
+          }
+          else{
+            console.log('i need to add user id  ')
+            result[0].reactions[req.body.loggedInUserId] = true; //this is how im adding star fav to post 
+            console.log(result[0].reactions)
+          }
+          
+          db.collection('journal-entry')
+          .findOneAndUpdate({_id: ObjectId(req.body.id)}, {
+            $set: {
+              reactions: result[0].reactions,
+            }
+          }, {
+            sort: {_id: -1},
+            upsert: true
+          }, (err, result) => {
+            if (err) return res.send(err)
+            res.send(result)
+          })
+        })
     })
 
-    app.delete('/messages', (req, res) => {
-      console.log("delete method")
-      db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+    app.delete('/community-page', (req, res) => {
+      console.log("delete method" , req.body)
+      db.collection('journal-entry').findOneAndDelete({_id: ObjectId(req.body.id)}, (err, result) => {
         if (err) return res.send(500, err)
-        res.send('Message deleted!')
+        res.send('Entry deleted!')
       })
     })
+
+
+  // =============================================================================
+  // JOURNAL ENTRY PAGE ENDPOINT ==================================================
+  // =============================================================================
+
+    /* Get Journal Entry Page and relevant data from DB */
+    app.get('/journal-entry', isLoggedIn, function(req, res) { // checks to see if logged in or send them back to homepage if not logged in 
+      console.log('journal-entry')
+        db.collection('journal-entry').find().toArray((err, result) => {
+          if (err) return console.log(err)
+          res.render('journal-entry.ejs', {
+            user : req.user//showcases there usr name when logged in
+          })
+        })
+    });
+
+
+    // POST JOURNAL ENTRY
+     app.post('/journal-entry', (req, res) => {
+      console.log("(PUT /journal-entry) : ")
+      console.log(req.body)
+      db.collection('journal-entry').save(
+        {
+          'entry-date': req.body['entry-date'], 
+          'fatigue-entry': req.body['fatigue-entry'], 
+          mood: req.body.mood, 
+          treatment: req.body.treatment, 
+          image: req.body.image,
+          description: req.body.description,
+          creatorId: req.body.creatorId,
+          reactions: {},
+          'warrior-name': req.body['warrior-name'],
+          'warrior-status': req.body['warrior-status'],
+          'caregiver-patient-name': req.body['caregiver-patient-name'],
+          'caregiver-patient-status': req.body['caregiver-patient-status'],
+          'relationship-type': req.body['relationship-type'],
+          'cancer-type' : req.body['cancer-type'],
+          'lang-type' : req.body['lang-type']
+        }, 
+        (err, result) => {
+          if (err) return console.log(err)
+          console.log('saved to database')
+          res.redirect('/journal-entry')
+      })
+    })
+
+
+// // message board routes ===============================================================
+
+//     app.post('/messages', (req, res) => {
+//       db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+//         if (err) return console.log(err)
+//         console.log('saved to database')
+//         res.redirect('/community-page')
+//       })
+//     })
+
+//     app.put('/messages', (req, res) => {
+
+//       let startCounterThumbUp = 0;
+
+//       console.log(" (put method) : ")
+//       console.log(req.body.thumbUp)
+//       if(req.body.thumbUp !== undefined){
+//       console.log("thumb up selected")
+//       startCounterThumbUp  = req.body.thumbUp == null ? 0 : req.body.thumbUp + 1;//ternary deals with like to handle "NaN" aka null in JS deal with it by adding 1 to it or setting the new list item to 0
+    
+//     } else if(req.body.thumbDown !== undefined ){
+
+//       console.log("thumb down(before): " + req.body.thumbDown)
+//       startCounterThumbUp = req.body.thumbDown == null ? 0 : req.body.thumbDown - 1;
+//       console.log("thumb down(after): " + startCounterThumbUp)
+
+//    //ternary deals with dislike to handle "NaN" aka null in JS deal with it by subtract 1 to it or setting the new list item to 0
+//       console.log("user selected thumb down");
+//     }
+//       db.collection('messages')
+//       .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+//         $set: {
+//           thumbUp:startCounterThumbUp,
+//         }
+//       }, {
+//         sort: {_id: -1},
+//         upsert: true
+//       }, (err, result) => {
+//         if (err) return res.send(err)
+//         res.send(result)
+//       })
+//     })
+
+//     app.delete('/messages', (req, res) => {
+//       console.log("delete method")
+//       db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+//         if (err) return res.send(500, err)
+//         res.send('Message deleted!')
+//       })
+//     })
 
 // =============================================================================
 // AUTHENTICATE (FIRST LOGIN) ==================================================
--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+
+
 // credit: https://stackoverflow.com/questions/66780265/need-a-form-field-to-be-shown-depending-on-the-results-of-another-field
 
 
@@ -21,81 +23,3 @@ document.querySelector("#entry-type").addEventListener("change", function(e) {
       //     break;
   }
 });
-
-
-
-
-
-
-// var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-// var thumbDown = document.getElementsByClassName("fa-thumbs-down");
-// var trash = document.getElementsByClassName("fa-trash-o");
-
-// Array.from(thumbUp).forEach(function(element) {
-//       element.addEventListener('click', function(){
-//         const name = this.parentNode.parentNode.childNodes[1].innerText
-//         const msg = this.parentNode.parentNode.childNodes[3].innerText
-//         const thumbUpCount = this.parentNode.parentNode.childNodes[5].innerText
-//         console.log('thumbUpCount(before) : ' + thumbUpCount)
-//         fetch('messages', {
-//           method: 'put',
-//           headers: {'Content-Type': 'application/json'},
-//           body: JSON.stringify({
-//             name: name,
-//             msg: msg,
-//             thumbUp:parseInt(thumbUpCount),
-//           })
-//         })
-//         .then(response => {
-//           if (response.ok) return response.json()
-//         })
-//         .then(data => {
-//           console.log(data)
-//           window.location.reload(true)
-//         })
-//       });
-// });
-
-// Array.from(thumbDown).forEach(function(element) {
-//       element.addEventListener('click', function(){
-//         const name = this.parentNode.parentNode.childNodes[1].innerText;
-//         const msg = this.parentNode.parentNode.childNodes[3].innerText;
-//         const thumbDownCount = this.parentNode.parentNode.childNodes[5].innerText;
-//         console.log('thumbDownCount(before) : ' + thumbDownCount);
-//         fetch('messages', {
-//           method: 'put',
-//           headers: {'Content-Type': 'application/json'},
-//           body: JSON.stringify({
-//             name: name,
-//             msg: msg,
-//             thumbDown: parseInt(thumbDownCount)
-//           }),
-//         })
-//         .then(response => {
-//           if (response.ok) return response.json()
-//         })
-//         .then(data => {
-//           console.log(data)
-//           window.location.reload(true)
-//         })
-//       });
-// });
-
-// Array.from(trash).forEach(function(element) {
-//       element.addEventListener('click', function(){
-//         const name = this.parentNode.parentNode.childNodes[1].innerText
-//         const msg = this.parentNode.parentNode.childNodes[3].innerText
-//         fetch('messages', {
-//           method: 'delete',
-//           headers: {
-//             'Content-Type': 'application/json'
-//           },
-//           body: JSON.stringify({
-//             'name': name,
-//             'msg': msg
-//           })
-//         }).then(function (response) {
-//           window.location.reload()
-//         })
-//       });
-// });
