664ce49ab1fcd0804b0d07994eaadca0dcd0c73a
socratic617 <erik.solis02135@gmail.com>
2023-11-18 22:17:30 -0500

    added psuedo code

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 // config/database.js
+//my credientials to find my database and collections
+//im using my module.export to share it amonst my other files 
 module.exports = {
 
     'url' : 'mongodb+srv://mongDB:happy123!@cluster0.c1bg23b.mongodb.net/circleofwarriors?retryWrites=true&w=majority', 
